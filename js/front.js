b9e758998f652df1bbfb2f3908e5086c172219c3
Abdul <diazabdulm@gmail.com>
2018-10-07 19:47:09 -0400

    added my about me; simplified app descriptions; cleaned stuff up

--- js/front.js
+++ js/front.js
@@ -23,19 +23,19 @@ $(document).ready(function () {
     var wunderlist = new design(
         'Wunderlist Redesign Concept',
         'https://mir-s3-cdn-cf.behance.net/project_modules/source/dce74646831391.5865d2a1c862e.png',
-        'I built this back when I used Wunderlist to manage my tasks. I loved Wunderlist, but disliked the look. So, I decided to design my own version. To this day, this is my most popular design.'
+        'I built this back when I used Wunderlist to manage my tasks. I loved Wunderlist, but heavily disliked the look. So, I decided to design my own version. To this day, this is my most popular design.'
     );
 
     var movieSite = new design(
         'Utopian Movie Site',
         'https://assets.materialup.com/uploads/ffd67f47-2726-4e76-8af2-5e93de39f097/preview.png',
-        'I was really interested in streaming movies. There was this site I loved but I felt that the interface was too old. I redesigned the site with a focus on ease of access.'
+        'I used to be heavy on streaming movies. There was this site I loved but I felt that the interface was too old. I redesigned the site with a focus on ease of access.'
     );
 
     var newspaper = new design(
         'Randolph Times Landing',
         'https://mir-s3-cdn-cf.behance.net/project_modules/source/b5b91d45644167.5837798f8d436.png',
-        'When I was in my high school\'s newspaper team, we didn\'t have an online presence. I decided to build our school a newspaper that they can access anywhere, anytime, online or offline. Unfortunately, the idea wasn\'t implemented but this design has been one of my favorites.'
+        'Back when I was in my high school\'s newspaper team, we didn\'t have an online presence. So, I decided to build our school a newspaper that they can access anywhere, anytime, online or offline. Unfortunately, the idea wasn\'t implemented but this design has been one of my favorites.'
     );
 
     var algebraCom = new design(
